b965b4ab760021f7872d0db72aae45b4cb6a63ec
annshopina <shopina.ann@gmail.com>
2025-02-02 22:01:18 +0100

    second commit

---
+++ src/logical.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = -2;
+let c = 32;
+let s1 = "hello world";
+let s2 = "bye world";
+let t = true;
+let f = false;
+
+let d = a===b ;
+console.log(d);
+
+let d1 = a===7 ;
+console.log(d);
