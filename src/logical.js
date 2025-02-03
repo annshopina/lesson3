5e8be97a95d4ac4924571921a1a9dc24134999e5
annshopina <shopina.ann@gmail.com>
2025-02-03 07:06:10 +0100

    fourth commit

--- src/logical.js
+++ src/logical.js
@@ -1,13 +1,29 @@
 let a = 5;
 let b = -2;
-let c = 32;
 let s1 = "hello world";
-let s2 = "bye world";
 let t = true;
 let f = false;
 
-let d = a===b ;
+let d = a === b;
 console.log(d);
 
-let d1 = a===7 ;
+let d1 = a === 7;
 console.log(d);
+
+let d2 = a !== b;
+console.log(d2);
+
+let d3 = a !== 7;
+console.log(d3);
+
+let d4 = s1 && f;
+console.log(d4);
+
+let d5 = s1 || b;
+console.log(d5);
+
+let d6 = s1 !== "hiiiiiii";
+console.log(d6);
+
+let d7 = t && true;
+console.log(d7);
